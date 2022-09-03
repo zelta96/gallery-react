fafbcff11cd9ba1f81d22a29ff632058a4143147
angel <victormedina9603@outlook.com>
2022-09-03 17:47:33 -0500

    a simple gallary for react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {Formik, Form, Field} from 'formik'
+import './header.css'
+import './content.css'
+import './articles.css'
 
-function App() {
+const App = () => {
+
+  const [photos,setPhotos] =useState([])
+  const open = url => window.open(url)
+  console.log(photos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <Formik
+          initialValues={{search:''}}
+          onSubmit={
+            async values =>{
+              const response =  await fetch(
+                `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`
+                ,{
+                  headers:{
+                    'Authorization' : 'Client-ID D66_csy4F_zpkYzmH3Kt1uAu1vDbaWB3_ECGh9-_VAI'
+                  }
+                }
+              )
+              const result = await response.json();
+              setPhotos(result.results)
+            }
+          }
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name='search'></Field>
+          </Form>
+        </Formik>
       </header>
+      <div className='container'>
+          <div className='center'>
+            {photos.map(
+              elem => 
+              <article key={elem.id} onClick={()=> open(elem.links.html)}>
+                <img src={elem.urls.regular} alt={elem.alt_description}/>
+                <p>{[elem.description,elem.alt_description].join('-')}</p>
+              </article>)}
+          </div>  
+      </div>
     </div>
   );
 }
